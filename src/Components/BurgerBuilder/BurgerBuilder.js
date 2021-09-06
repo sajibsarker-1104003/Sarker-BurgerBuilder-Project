19b7f226a662b7ca0c05e65afee1961c00b50293
sajibsarker-1104003 <sajibpranto600@gmail.com>
2021-09-06 21:59:09 +0600

    5.Controls Component

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,20 +1,21 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import Burger from './Burger/Burger';
-
+import Controls from './Controls/Controls';
 
 export default class BurgerBuilder extends Component {
-  state={
-    ingredients:[       
-      {type:'salad',amount:0},
-      {type:'cheese',amount:0},
-      {type:'meat',amount:0}
-
-    ]
-  }
-  render(){
-    return(
-      <div>
-        <Burger ingredients={this.state.ingredients}/>
-      </div>)
-  }
+    state = {
+        ingredients: [
+            { type: 'salad', amount: 0 },
+            { type: 'cheese', amount: 0 },
+            { type: 'meat', amount: 0 },
+        ]
+    }
+    render() {
+        return (
+            <div className="d-flex flex-md-row flex-column">
+                <Burger ingredients={this.state.ingredients} />
+                <Controls />
+            </div>
+        )
+    }
 }
